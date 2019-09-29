5c85c38cfd31878808e28343e66df187816a2d94
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-09-29 13:53:36 +0100

    Set up express & connected to mongo database

---
+++ index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const app = require('./src/app');
+
+mongoose.connect(process.env.DATABASE_CONN, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, (err) => {
+  console.log(err);
+  app.listen(3000);
+});
