b474a92ccd8dd2ab033f8d50f9e2df99cbbad5b6
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-10-02 20:37:20 +0100

    added functionality to post - user can be added to database

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userController = require('./controllers/user');
 
 const app = express();
 app.use(express.json());
@@ -6,5 +7,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
 });
+
+app.post('/users', userController.create);
 
 module.exports = app;
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,4 +1,5 @@
 const User = require('../src/models/user');
+const chai = require('chai');
 
 describe('Users', () => {
   beforeEach((done) => {
@@ -21,8 +22,8 @@ describe('Users', () => {
           expect(err).to.equal(null);
           expect(res.status).to.equal(201);
 
-          User.findbyId(res.body._id, (err, user) => {
-            expect(err).to.equal(null);
+          User.findById(res.body._id, (error, user) => {
+            expect(error).to.equal(null);
             expect(user.firstName).to.equal('Holly');
             expect(user.lastName).to.equal('Fanthorpe');
             expect(user.email).to.equal('hollyfanthorpe@gmail.com');
