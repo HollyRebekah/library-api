f7d07e218639ec856b4f95f0e032ed0fabfed65d
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-10-06 09:31:04 +0100

    refactored tests using data factory

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,6 @@
 const User = require('../src/models/user');
 const chai = require('chai');
+const DataFactory = require('./helpers/data-factory');
 
 describe('Users', () => {
   beforeEach((done) => {
@@ -9,25 +10,21 @@ describe('Users', () => {
   });
 
   describe('POST /users', () => {
+    const userInfo = DataFactory.user();
     it('creates a new user in the database and encrypts the password', (done) => {
       chai.request(server)
         .post('/users')
-        .send({
-          firstName: 'Holly',
-          lastName: 'Fanthorpe',
-          email: 'hollyfanthorpe@gmail.com',
-          password: 'password123',
-        })
+        .send(userInfo)
         .end((err, res) => {
           expect(err).to.equal(null);
           expect(res.status).to.equal(201);
 
           User.findById(res.body._id, (error, user) => {
             expect(error).to.equal(null);
-            expect(user.firstName).to.equal('Holly');
-            expect(user.lastName).to.equal('Fanthorpe');
-            expect(user.email).to.equal('hollyfanthorpe@gmail.com');
-            expect(user.password).not.equal('password123');
+            expect(user.firstName).to.equal(userInfo.firstName);
+            expect(user.lastName).to.equal(userInfo.lastName);
+            expect(user.email).to.equal(userInfo.email);
+            expect(user.password).not.equal(userInfo.password);
             expect(user.password).to.have.lengthOf(60);
             expect(res.body).not.to.have.property('password');
             done();
@@ -36,14 +33,10 @@ describe('Users', () => {
     });
 
     it('checks for a valid email address', (done) => {
+      const userInfoWrongEmail = DataFactory.user({ email: 'email' });
       chai.request(server)
         .post('/users')
-        .send({
-          firstName: 'Holly',
-          lastName: 'Fanthorpe',
-          email: 'hollyfanthorpe',
-          password: 'password123',
-        })
+        .send(userInfoWrongEmail)
         .end((err, res) => {
           expect(err).to.equal(null);
           expect(res.status).to.equal(400);
@@ -57,14 +50,10 @@ describe('Users', () => {
     });
 
     it('checks for a valid password', (done) => {
+      const userInfoWrongPassword = DataFactory.user({ password: 'p' });
       chai.request(server)
         .post('/users')
-        .send({
-          firstName: 'Holly',
-          lastName: 'Fanthorpe',
-          email: 'hollyfanthorpe',
-          password: 'p',
-        })
+        .send(userInfoWrongPassword)
         .end((err, res) => {
           expect(err).to.equal(null);
           expect(res.status).to.equal(400);
